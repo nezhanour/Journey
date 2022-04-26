b4816cbd875805ee1c31c7eac8131e6a0ba33c2b
nezhanour <nezha95nour@gmail.com>
2022-04-26 18:38:07 +0000

    Updating exercise description

--- Day8/create_file.py
+++ Day8/create_file.py
@@ -1,3 +1,5 @@
+#Write all content of a given file into a new file by skipping line number 5
+
 with open('test.txt', 'r') as test:
     lines = test.readlines()
     new = open('new_file.txt', "w")

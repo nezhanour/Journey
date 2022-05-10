078e7252e1887febe6f5fdbaff732e5d92202c0b
nezhanour <nezha95nour@gmail.com>
2022-05-10 20:12:36 +0100

    Adding comments on total digits exercise

--- Day12/Display_list_numbers.py
+++ Day12/Display_list_numbers.py
@@ -7,9 +7,12 @@
 numbers = [12, 75, 150, 180, 145, 525, 50]
 
 for num in numbers:
+    #checking if the number is greater than 500 if yes then the loop stop
     if num > 500:
         break
+    #checking if the number is greater than 15O if yes then we skip to the next and move to the next number
     elif num > 150:
         continue
+    #checking if the number is divisible by five if yes we print the number
     elif num % 5 == 0:
         print(num)
